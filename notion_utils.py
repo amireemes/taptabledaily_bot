e6fc1639907e1b6b8cdc84fc99938ceac3222dc7
Yergaliyev Amir <92968384+amireemes@users.noreply.github.com>
2025-07-26 09:35:22 +0500

    Update notion_utils.py

--- notion_utils.py
+++ notion_utils.py
@@ -18,13 +18,13 @@ NOTION_DATABASE_ID = "23cf33d443d98012af65f0b879d550e1"
 
 # Привязка Telegram ID к пользователю в Notion
 NOTION_PEOPLE = {
-    524373106: {"name": "Amir Yergaliyev", "id": "77e429c5-71bb-4fb6-81c3-4c6ae041afe7"}
+    524373106: {"name": "Amir Yergaliyev", "id": "77e429c5-71bb-4fb6-81c3-4c6ae041afe7"},
     501421236: {"name": "Temirlan Ismagulov", "id": "1faeb36b-21be-47f1-8720-199e1f4079e8"},
     385608549: {"name": "Alemkhan Yergaliyev", "id": "376ad707-ec57-4bf3-9376-765ee237b4eb"},
     1224720716: {"name": "Damir Kushembayev", "id": "207d872b-594c-81c5-abe9-00024958601a"},
     897190202: {"name": "Bekzhan Aktoreev", "id": "ebbd2e0d-d368-46aa-b639-1b4bb7007425"},
     501352218: {"name": "Daniyal Serik", "id": "f3a889de-9993-4ec2-ace5-dbee0fcbaaac"},
-    5006534774: {"name": "Abdulla Jurayev", "id": "48e3c66b-f93b-4dbf-a1a9-30f9d348532f"},
+    5006534774: {"name": "Abdulla Jurayev", "id": "48e3c66b-f93b-4dbf-a1a9-30f9d348532f"}
 }
 
 def format_date():
